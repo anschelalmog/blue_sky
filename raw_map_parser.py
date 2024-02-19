d1bad85f175695407b4df6c6d2aab3084ea762a6
anschelalmog <anschelalmog@gmail.com>
2024-02-19 22:30:08 +0200

    19.2

--- raw_map_parser.py
+++ raw_map_parser.py
@@ -4,9 +4,15 @@ import rasterio
 from scipy.io import savemat
 import matplotlib.pyplot as plt
 
-with rasterio.open('C:/Users/ALMOGAN/Downloads/gt30e020n40_dem/gt30e020n40.dem') as dem:
+# path to dem file should contain other data map data in the folder
+laptop_path_dem = 'C:/Users/ALMOGAN/Downloads/gt30e020n40_dem/gt30e020n40.dem'
+laptop_path_out = 'C:/Users/ALMOGAN/Documents/blue_sky/Map/Level1/DTED/'
+
+with rasterio.open(laptop_path_dem) as dem:
     elevation_data = dem.read(1)
-show_details = True
+
+show_details = False
+break_and_save = True
 
 if show_details:
     print(elevation_data.shape)
@@ -17,7 +23,6 @@ if show_details:
     plt.title('Elevation Map')
     plt.show()
 
-break_and_save = True
 if break_and_save:
     elevation_data[elevation_data == -9999] = 0
     elevation_data[elevation_data < 0] = np.abs(elevation_data[elevation_data < 0])
@@ -29,7 +34,7 @@ if break_and_save:
     num_sections_y = elevation_data.shape[0] // (section_size - 1) - 1
 
     # Create the directories and save the .mat files
-    base_path = 'C:/Users/ALMOGAN/Documents/blue_sky/Map/Level1/DTED/'
+    base_path = laptop_path_out
 
     for i in range(num_sections_y):
         for j in range(num_sections_x):
@@ -42,19 +47,15 @@ if break_and_save:
             section = elevation_data[start_y:end_y, start_x:end_x]
 
             # Calculate the latitude and longitude for the section
-            lat = 40 - i  # Assuming each section corresponds to a 1 degree change in latitude
-            lon = 20 + j  # Assuming each section corresponds to a 1 degree change in longitude
+            lat = 40 - i
+            lon = 20 + j
 
-            # Create the directory name based on the longitude
             dir_name = f'E{lon:03d}'
             dir_path = os.path.join(base_path, dir_name)
-            # Make the directory if it doesn't exist
             os.makedirs(dir_path, exist_ok=True)
 
             # Define the filename based on the latitude
             file_name = f'n{lat:02d}.mat'
             file_path = os.path.join(dir_path, file_name)
 
-            # Save the section as a .mat file
             savemat(file_path, {'elevation_data': section})
-
