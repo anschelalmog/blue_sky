3c6c4ea97bc31175e79a1f6891edd0078ddc5845
anschelalmog <anschelalmog@gmail.com>
2024-03-27 23:58:56 +0200

    updated UKF class

--- src/estimators.py
+++ src/estimators.py
@@ -189,7 +189,7 @@ class IEKF:
         # Assuming linear change in Euler angles
         self.traj.euler.psi[i] = self.traj.euler.psi[i - 1] + ((meas.euler.psi[i] - meas.euler.psi[i - 1]) * self.del_t)
         self.traj.euler.theta[i] = self.traj.euler.theta[i - 1] + (
-                    (meas.euler.theta[i] - meas.euler.theta[i - 1]) * self.del_t)
+                (meas.euler.theta[i] - meas.euler.theta[i - 1]) * self.del_t)
         self.traj.euler.phi[i] = self.traj.euler.phi[i - 1] + ((meas.euler.phi[i] - meas.euler.phi[i - 1]) * self.del_t)
 
         self.traj.vel.north[i] = self.traj.vel.north[i - 1] + meas.vel.north[i] - meas.vel.north[i - 1]
@@ -340,10 +340,8 @@ class IEKF:
            - converged (bool): True if the state estimate has converged, False otherwise.
            """
         i = self.curr_state
-        loss = np.linalg.norm(self.params.dX[:, i] - self.params.dX[:, i-1])
+        loss = np.linalg.norm(self.params.dX[:, i] - self.params.dX[:, i - 1])
         return loss < self.conv_rate
-
-
 
 
 class IEKFParams:
@@ -373,22 +371,40 @@ class UKF:
         self.curr_state = None
         self.params = UKFParams(self)  # UKF parameters
         self.traj = BaseTraj(args.run_points)
+        self.measurement_size = 1
 
     def run(self, map_data, meas):
-        for i in range(self.run_points):
+        self._initialize_traj(meas)
+        self._initialize_params()
+
+        desc = "Estimating Trajectory with UKF"
+        for i in tqdm(range(1, self.run_points), desc=desc):
             self.curr_state = i
 
             # Predict step
             X_sigma = self._compute_sigma_points()
-            X_sigma_pred = self._predict_sigma_points(X_sigma)
+            X_sigma_pred = self._predict_sigma_points(X_sigma, meas)
             self._predict_state(X_sigma_pred)
-            self._predict_covariance(X_sigma_pred)
+            P_pred = self._predict_covariance(X_sigma_pred)
 
             # Update step
-            Z_sigma = self._unscented_transform(X_sigma_pred)
+            lat, lon = self._pinpoint_coordinates(meas)
+            self._find_slopes(lat, lon, P_pred, map_data)  # updates SN, SE, Rfit
+            h_asl_meas = meas.pos.h_asl[i]
+            jac = cosd(meas.euler.theta[i]) * cosd(meas.euler.phi[i])
+            h_agl_meas = meas.pinpoint.range[i] * jac
+            h_map_meas = interp2d(map_data.axis['lon'], map_data.axis['lat'], map_data.grid)(lon, lat)
+            self.traj.pos.h_agl[i] = h_agl_meas
+
+            self._calc_rc(h_agl_meas)
+            self.params.R[i] = self.params.Rc[i] + self.params.Rfit[i]  # calc noise
+
+            Z_sigma = self._unscented_transform(X_sigma_pred, map_data)
             Z_pred = self._compute_predicted_measurement(Z_sigma)
-            self._update_state(Z_sigma, Z_pred, meas)
+            self._update_state(X_sigma_pred, Z_sigma, Z_pred, h_asl_meas, h_agl_meas, h_map_meas, meas)
             self._update_covariance(X_sigma_pred, Z_sigma, Z_pred)
+
+        return self
 
     def _predict_state(self, X_sigma_pred):
         # Predict the state estimate based on sigma points
@@ -397,28 +413,56 @@ class UKF:
             x_pred += self.params.weights_mean[i] * X_sigma_pred[:, i]
         return x_pred
 
-    def _predict_sigma_points(self, X_sigma):
-        # Propagate each sigma point through the process model
-        # Assuming a linear process model for demonstration purposes
+    def _predict_sigma_points(self, X_sigma, meas):
+        i = self.curr_state
         X_sigma_pred = np.zeros_like(X_sigma)
-        for i in range(2 * self.state_size + 1):
-            X_sigma_pred[:, i] = self._process_model(X_sigma[:, i])
+        for j in range(2 * self.state_size + 1):
+            X_sigma_pred[0, j] = X_sigma[0, j] + (
+                        X_sigma[3, j] * self.del_t + 0.5 * meas.acc.north[i - 1] * self.del_t ** 2) / meas.mpd_north[i]
+            X_sigma_pred[1, j] = X_sigma[1, j] + (
+                        X_sigma[4, j] * self.del_t + 0.5 * meas.acc.east[i - 1] * self.del_t ** 2) / meas.mpd_east[i]
+            X_sigma_pred[2, j] = X_sigma[2, j] + (
+                        X_sigma[5, j] * self.del_t + 0.5 * meas.acc.down[i - 1] * self.del_t ** 2)
+            X_sigma_pred[3, j] = X_sigma[3, j] + meas.acc.north[i - 1] * self.del_t
+            X_sigma_pred[4, j] = X_sigma[4, j] + meas.acc.east[i - 1] * self.del_t
+            X_sigma_pred[5, j] = X_sigma[5, j] + meas.acc.down[i - 1] * self.del_t
+            X_sigma_pred[6:9, j] = X_sigma[6:9, j]  # Accelerations remain the same
+            X_sigma_pred[9, j] = X_sigma[9, j] + ((meas.euler.psi[i] - meas.euler.psi[i - 1]) * self.del_t)
+            X_sigma_pred[10, j] = X_sigma[10, j] + ((meas.euler.theta[i] - meas.euler.theta[i - 1]) * self.del_t)
+            X_sigma_pred[11, j] = X_sigma[11, j] + ((meas.euler.phi[i] - meas.euler.phi[i - 1]) * self.del_t)
         return X_sigma_pred
 
-    def _predict_covariance(self, X_sigma_pred):
-        # Predict the state covariance estimate based on sigma points
-        P_pred = np.zeros((self.state_size, self.state_size))
-        x_pred = self._predict_state(X_sigma_pred)
-        for i in range(2 * self.state_size + 1):
-            diff = X_sigma_pred[:, i] - x_pred
-            P_pred += self.params.weights_covariance[i] * np.outer(diff, diff)
-        return P_pred
+    def _update_state(self, X_sigma_pred, Z_sigma, Z_pred, h_asl_meas, h_agl_meas, h_map_meas, meas):
+        i = self.curr_state
+        Pxz = self._compute_cross_covariance(X_sigma_pred, Z_sigma, Z_pred)
+        Pzz = self._compute_innovation_covariance(Z_sigma, Z_pred)
+        K = Pxz @ np.linalg.pinv(Pzz)
 
-    def _update_state(self, Z_sigma, Z_pred, meas):
-        pass  # State update step
+        innovation = h_asl_meas - h_agl_meas - h_map_meas - Z_pred
+        self.params.dX[:, i] = K @ innovation
+
+        self.traj.pos.lat[i] -= self.params.dX[0, i] / meas.mpd_north[i]
+        self.traj.pos.lon[i] -= self.params.dX[1, i] / meas.mpd_east[i]
+        self.traj.pos.h_asl[i] -= self.params.dX[2, i]
+        self.traj.vel.north[i] -= self.params.dX[3, i]
+        self.traj.vel.east[i] -= self.params.dX[4, i]
+        self.traj.vel.down[i] -= self.params.dX[5, i]
+        self.traj.acc.north[i] -= self.params.dX[6, i]
+        self.traj.acc.east[i] -= self.params.dX[7, i]
+        self.traj.acc.down[i] -= self.params.dX[8, i]
+        self.traj.euler.psi[i] -= self.params.dX[9, i]
+        self.traj.euler.theta[i] -= self.params.dX[10, i]
+        self.traj.euler.phi[i] -= self.params.dX[11, i]
+        self.traj.pos.h_map[i] = self.traj.pos.h_asl[i] - self.traj.pos.h_agl[i]
+        self.traj.pos.north[i] = self.traj.pos.lat[i] * meas.mpd_north[i]
+        self.traj.pos.east[i] = self.traj.pos.lon[i] * meas.mpd_east[i]
 
     def _update_covariance(self, X_sigma_pred, Z_sigma, Z_pred):
-        pass  # Covariance update step
+        i = self.curr_state
+        Pxz = self._compute_cross_covariance(X_sigma_pred, Z_sigma, Z_pred)
+        Pzz = self._compute_innovation_covariance(Z_sigma, Z_pred)
+        K = Pxz @ np.linalg.pinv(Pzz)
+        self.params.P_est[:, :, i] = self._predict_covariance(X_sigma_pred) - K @ Pzz @ K.T
 
     def _compute_sigma_points(self):
         # Compute the sigma points for the current state
@@ -436,10 +480,15 @@ class UKF:
 
         return sigma_points
 
-    def _unscented_transform(self, X_sigma_pred):
-        Z_sigma = np.zeros((self.measurement_size, 2 * self.state_size + 1))
-        for i in range(2 * self.state_size + 1):
-            Z_sigma[:, i] = self._measurement_model(X_sigma_pred[:, i])
+    def _unscented_transform(self, X_sigma_pred, map_data):
+        i = self.curr_state
+        Z_sigma = np.zeros((1, 2 * self.state_size + 1))
+        for j in range(2 * self.state_size + 1):
+            jac = cosd(X_sigma_pred[10, j]) * cosd(X_sigma_pred[11, j])
+            h_agl_pred = self.traj.pinpoint.range[i] * jac
+            h_map_pred = interp2d(map_data.axis['lon'], map_data.axis['lat'], map_data.grid)(X_sigma_pred[1, j],
+                                                                                             X_sigma_pred[0, j])
+            Z_sigma[0, j] = X_sigma_pred[2, j] - h_agl_pred - h_map_pred
         return Z_sigma
 
     def _compute_predicted_measurement(self, Z_sigma):
@@ -449,54 +498,191 @@ class UKF:
             z_pred += self.params.weights_mean[i] * Z_sigma[:, i]
         return z_pred
 
+    def _compute_cross_covariance(self, X_sigma_pred, Z_sigma, Z_pred):
+        Pxz = np.zeros((self.state_size, self.measurement_size))
+        for i in range(2 * self.state_size + 1):
+            Pxz += self.params.weights_covariance[i] * np.outer(X_sigma_pred[:, i] - self._predict_state(X_sigma_pred),
+                                                                Z_sigma[:, i] - Z_pred)
+        return Pxz
+
+    def _compute_innovation_covariance(self, Z_sigma, Z_pred):
+        Pzz = np.zeros((self.measurement_size, self.measurement_size))
+        for i in range(2 * self.state_size + 1):
+            Pzz += self.params.weights_covariance[i] * np.outer(Z_sigma[:, i] - Z_pred, Z_sigma[:, i] - Z_pred)
+        Pzz += self.params.R[self.curr_state]
+        return Pzz
+
+    def _initialize_params(self):
+        self.params = UKFParams(self)
+        self.params.P_est[:, :, 0] = np.power(np.diag([200, 200, 30, 2, 2, 2, 1, 1, 1, 1, 1, 1]), 2)
+        self.params.Q = np.power(np.diag([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), 1e-7)
+
+    def _initialize_traj(self, meas):
+        """
+        Set initial values from measurements
+
+        """
+
+        self.traj.pos.lat[0] = meas.pos.lat[0]
+        self.traj.pos.lon[0] = meas.pos.lon[0]
+        self.traj.pos.h_asl[0] = meas.pos.h_asl[0]  # altitude
+        jac = cosd(meas.euler.theta[0]) * cosd(meas.euler.phi[0])
+        self.traj.pos.h_agl[0] = meas.pinpoint.range[0] * jac  # H_agl_p
+        #
+        self.traj.vel.north[0] = meas.vel.north[0]
+        self.traj.vel.east[0] = meas.vel.east[0]
+        self.traj.vel.down[0] = meas.vel.down[0]
+        #
+        self.traj.pinpoint.delta_north[0] = meas.pinpoint.delta_north[0]
+        self.traj.pinpoint.delta_east[0] = meas.pinpoint.delta_east[0]
+        self.traj.pinpoint.h_map[0] = meas.pinpoint.h_map[0]
+        #
+        self.traj.euler.psi[0] = meas.euler.psi[0]
+        self.traj.euler.theta[0] = meas.euler.theta[0]
+        self.traj.euler.phi[0] = meas.euler.phi[0]
+
+    def _find_slopes(self, lat, lon, p_pre, map_data):
+        """
+        calculate the north-south and east-west slopes of terrain at a given lat lon,
+        and estimate the Error of the slope model
+
+        :param lat: Latitude at point [deg]
+        :param lon: Longitude at point [deg]
+        :param p_pre: A priori Error covariance matrix
+        :param map_data: MapLoad instance that contain the grid and it's axes
+        :return: SN, SE: slopes values , RFIT: fit Error
+        """
+        i = self.curr_state
+        dP = 100  # distance increments in [m]
+        delPmap = np.array([dP / map_data.mpd['north'][i], dP / map_data.mpd['east'][i]])  # [deg]
+
+        # max number of points in each direction
+        maxP = np.sqrt(max(p_pre[0][0], p_pre[1][1]))
+        KP = 3
+        NC = np.ceil(max(KP, 2 * np.ceil(KP * maxP / dP) + 1) / 2)
+        idx = (NC - 1) / 2  # indices
+
+        # create lat lon vectors according to grid indices
+        pos_offset = np.arange(-idx, idx + 1)
+        lat_vec, lon_vec = delPmap[0] * pos_offset + lat, delPmap[0] * pos_offset + lon
+        xp, yp = np.meshgrid(lon_vec, lat_vec)
+        xp, yp = xp[0, :], yp[:, 0]
+        # scaling factors for slope calc
+        sx2 = (dP ** 2) * 2 * NC * np.sum(np.power(np.arange(1, idx + 1), 2))
+        sy2 = sx2
+
+        # interpolate elevation data for current location
+        interpolator = interp2d(map_data.axis['lon'], map_data.axis['lat'], map_data.grid)
+        ref_elevation = interpolator(lon, lat)[0]
+        grid_elevations = interpolator(xp, yp)
+
+        # calculate slopes in x and y directions
+        syh = dP * np.dot(pos_offset, grid_elevations - ref_elevation).sum()
+        sxh = dP * np.dot(grid_elevations - ref_elevation, pos_offset).sum()
+
+        self.params.SN[i], self.params.SE[i] = sxh / sx2, syh / sy2
+
+        SN, SE = self.params.SN[i], self.params.SE[i]
+        MP = (2 * idx + 1) ** 2  # number of points in the mesh grid
+        In = 0
+        # calculate the Error over the grid
+        for step_E in np.arange(-idx, idx + 1):
+            for step_N in np.arange(-idx, idx + 1):
+                north = int(step_N + idx)
+                east = int(step_E + idx)
+                In = In + (dP * (SN * step_N + SE * step_N) - grid_elevations[east][north] + ref_elevation) ** 2
+        self.params.Rfit[i] = In / (MP - 1)
+
+    def _calc_rc(self, h_agl_meas):
+        self.params.Rc[self.curr_state] = 100 + 125 * (h_agl_meas > 200) + 175 * (h_agl_meas > 760) + 600 * (
+                h_agl_meas > 1000) + 500 * (
+                                                  h_agl_meas > 5000) + 1500 * (h_agl_meas > 7000)
+
+    def _predict_covariance(self, X_sigma_pred):
+        x_pred = self._predict_state(X_sigma_pred)
+        P_pred = np.zeros((self.state_size, self.state_size))
+        for i in range(2 * self.state_size + 1):
+            diff = X_sigma_pred[:, i] - x_pred
+            P_pred += self.params.weights_covariance[i] * np.outer(diff, diff)
+        P_pred += self.params.Q
+        return P_pred
+
+    def _get_current_state_estimate(self):
+        i = self.curr_state
+        return np.array([
+            self.traj.pos.lat[i],
+            self.traj.pos.lon[i],
+            self.traj.pos.h_asl[i],
+            self.traj.vel.north[i],
+            self.traj.vel.east[i],
+            self.traj.vel.down[i],
+            self.traj.acc.north[i],
+            self.traj.acc.east[i],
+            self.traj.acc.down[i],
+            self.traj.euler.psi[i],
+            self.traj.euler.theta[i],
+            self.traj.euler.phi[i]
+        ])
+
+    def _get_current_covariance_estimate(self):
+        return self.params.P_est[:, :, self.curr_state]
+
+    def _pinpoint_coordinates(self, meas):
+        """
+        calculate current lat, lon
+
+        :param meas:
+        :return:
+        """
+        i = self.curr_state
+        psi = meas.euler.psi[i]
+        theta = meas.euler.theta[i]
+        phi = meas.euler.phi[i]
+
+        self.traj.pinpoint.delta_north[i] = meas.pinpoint.range[i] * (
+                cosd(psi) * sind(theta) * cosd(phi) + sind(psi) * sind(phi))
+        self.traj.pinpoint.delta_east[i] = meas.pinpoint.range[i] * (
+                sind(psi) * sind(theta) * cosd(phi) - cosd(psi) * sind(phi))
+
+        # lon - lat at pinpoint
+        lat = self.traj.pos.lat[i] + self.traj.pinpoint.delta_north[i] / meas.mpd_north[i]
+        lon = self.traj.pos.lon[i] + self.traj.pinpoint.delta_east[i] / meas.mpd_east[i]
+
+        return lat, lon
 
 class UKFParams:
     def __init__(self, ukf):
-        # State size and run points from the UKF instance
         self.state_size = ukf.state_size
         self.run_points = ukf.run_points
+        self.P_est = np.zeros((self.state_size, self.state_size, self.run_points))
+        self.Q = np.zeros((self.state_size, self.state_size))
+        self.R = np.zeros(self.run_points)
+        self.Rc = np.zeros(self.run_points)
+        self.Rfit = np.zeros(self.run_points)
+        self.dX = np.zeros((self.state_size, self.run_points))
+        self.SN = np.zeros(self.run_points)
+        self.SE = np.zeros(self.run_points)
 
-        # Covariance matrices
-        self.P_est = np.zeros((self.state_size, self.state_size, self.run_points))  # Estimated error covariance
-        self.Q = np.zeros((self.state_size, self.state_size))  # Process noise covariance
-        self.R = np.zeros(self.run_points)  # Measurement noise covariance
-
-        # UKF  parameters # should be args
         self.alpha = 1e-3
         self.beta = 2
         self.kappa = 0
-
-        # Computed parameters
         self.lambda_ = self._compute_lambda()
         self.gamma = np.sqrt(self.state_size + self.lambda_)
-
-        # Sigma points weights
-        self.weights_mean = self._compute_weights_mean()  # Weights for the mean
-        self.weights_covariance = self._compute_weights_covariance()  # Weights for the covariance
+        self.weights_mean = self._compute_weights_mean()
+        self.weights_covariance = self._compute_weights_covariance()
 
     def _compute_lambda(self):
-        """
-        Compute lambda using alpha, kappa, and state size.
-        """
         return self.alpha ** 2 * (self.state_size + self.kappa) - self.state_size
 
     def _compute_weights_mean(self):
-        """
-        Compute weights for the sigma points mean.
-        """
         weights = np.full(2 * self.state_size + 1, 1 / (2 * (self.state_size + self.lambda_)))
         weights[0] = self.lambda_ / (self.state_size + self.lambda_)
         return weights
 
     def _compute_weights_covariance(self):
-        """
-        Compute weights for the sigma points covariance.
-        """
         weights = np.full(2 * self.state_size + 1, 1 / (2 * (self.state_size + self.lambda_)))
         weights[0] = self.lambda_ / (self.state_size + self.lambda_) + (1 - self.alpha ** 2 + self.beta)
         return weights
-
-
 """
 def _update_estimate_state(self, meas):
     # Update estimated state
