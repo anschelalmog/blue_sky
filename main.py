2dfdd7b98575f396c1dbb6ed0135dd8f71ff7869
ALMOGAN <anschelalmog@gmail.com>
2024-05-07 11:08:13 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import sys
-import time
-from matplotlib import pyplot as plt
-import os
-import numpy as np
-
 from src.data_loaders import *
 from src.create_traj import *
 from src.noise_traj import *
 from src.estimators import *
 from src.outputs_utils import *
-
 
 if __name__ == '__main__':
     args = set_settings()  # Set the system settings
@@ -24,13 +17,16 @@ if __name__ == '__main__':
     # Generate a noisy trajectory to simulate the sensor measurements
     meas_traj = NoiseTraj(true_traj).add_noise(errors.imu_errors, dist=args.noise_type, approach='top-down')
 
-    # Estimation of trajectory
+    # Perform trajectory estimation based on the selected Kalman filter type
     if args.kf_type == 'IEKF':
         # runs Iterated Extended Kalman Filter
         estimation_results = IEKF(args).run(map_data, meas_traj)
     else:  # args.kf_type == 'UKF':
         # runs Unscented Kalman Filter
         estimation_results = UKF(args).run(map_data, meas_traj)
+
+    # Calculate errors and covariances between the used trajectory and the estimated trajectory
+    errors, covariances = calc_errors_covariances(true_traj or meas_traj, estimation_results)
 
     args.plots = {
         'plot map': True,
@@ -42,10 +38,8 @@ if __name__ == '__main__':
         'kalman gains': True,
         'map elevation': True,
     }
-
-    #
-    errors, covariances = calc_errors_covariances(true_traj or meas_traj, estimation_results)
-
-    # results output
+    # Plot the results based on the plotting options
     plot_results(args, map_data, true_traj, meas_traj, estimation_results, errors, covariances)
+
+    # Print log information including settings, estimation results, errors, and covariances
     print_log(args, estimation_results, errors, covariances)
